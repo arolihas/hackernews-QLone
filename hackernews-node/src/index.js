22c52f448387e21047ebe77afaa5a7264b7ca518
arolihas <arolihas@gmail.com>
2020-05-24 19:34:42 -0400

    Reorganized frontend structure and added CRUD functionality to API

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,19 +1,47 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-	info: String!
-}
-`
+let links = [{
+	id: 'link-0',
+	url: 'www.howtographql.com',
+	description: 'Fullstack tutorial for GraphQL'
+}]
+let idCount = links.length
 
 const resolvers = {
-	Query: {
-		info: () => null,//'This is the API of the Hackernews Clone'
-	}
+  Query: {
+    info: () => 'This is the API of the Hackernews Clone',
+    feed: () => links,
+    link: (id) => links[id],
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+	description: args.description,
+	url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    update: (parent, args) => {
+      toUpdate = links.find(e => e.id === args.id)
+      const newLink = {
+        id: args.id,
+	url: args.url ? args.url : toUpdate.url,
+	description: args.description ? args.description : toUpdate.description
+      }
+      links[links.indexOf(toUpdate)] = newLink 
+      return newLink
+    },
+    delete: (parent, args) => {
+      deleted = links.find(e => e.id === args.id)
+      links = links.filter(e => e.id !== args.id)
+      return deleted
+    }
+  },
 }
-
 const server = new GraphQLServer({
-	typeDefs,
+	typeDefs: './src/schema.graphql',
 	resolvers,
 })
 
